8fdd16ee68945598c15db658871438869d6c20fa
youi0017 <37988671+youi0017@users.noreply.github.com>
2020-11-10 10:44:48 +0800

    Create demo.vue
    vue-imgLooper 使用demo-v1

---
+++ demo.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 class="text-center">
+      Vue轮播图组件使用示例
+    </h1>
+    
+	<imglooper height="250px" interval="2000"></imglooper>
+</template>
+
+<script>
+import imglooper  from "../../components/Lunbo.vue";
+
+export default {
+  data () {
+    return {
+      msg: 'chy 2020-11'
+    }
+  },
+  components:{
+	 imglooper
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
